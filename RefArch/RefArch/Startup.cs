cce5b4b327a053ff58a409a3e77045fd459de3df
cedric <ceschool@gmail.com>
2015-05-10 02:11:58 +0200

    Push solution

---
+++ RefArch/RefArch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RefArch.Startup))]
+namespace RefArch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
